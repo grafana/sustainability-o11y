1d35f17ced0cb67296413ab0a9f5d278ffc8ef61
Cindy Pallares <5558813+cindy@users.noreply.github.com>
2026-03-06 23:39:31 +0000

    feat(gcp): Add option to autocreate grafana datasource service account in module

--- terraform/modules/gcp-carbon-pipeline/iam.tf
+++ terraform/modules/gcp-carbon-pipeline/iam.tf
@@ -1,5 +1,3 @@
-# Permissions to initiate a carbon data transfer at the project level.
-# Only created when org_id is provided.
 resource "google_organization_iam_custom_role" "bigquery_transfer_climate_data_project" {
   count       = var.org_id != null ? 1 : 0
   role_id     = "BigQueryTransferClimateProject"
@@ -25,4 +23,11 @@ resource "google_organization_iam_member" "climate_billing_role" {
   org_id = var.org_id
   role   = "roles/billing.carbonViewer"
   member = google_service_account.gcp_climate_data.member
+}
+
+resource "google_project_iam_member" "grafana_bigquery_job_user" {
+  count   = var.grafana_bigquery_data_source ? 1 : 0
+  project = var.project_id
+  role    = "roles/bigquery.jobUser"
+  member  = google_service_account.grafana_bigquery_data_source[0].member
 }
