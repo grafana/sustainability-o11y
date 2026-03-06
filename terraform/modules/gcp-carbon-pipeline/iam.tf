897c562af076b6efc43e5202e60bc92da6eae89b
Cindy Pallares <5558813+cindy@users.noreply.github.com>
2026-03-06 22:09:09 +0000

    Update to use existing "carbonViewer" role

--- terraform/modules/gcp-carbon-pipeline/iam.tf
+++ terraform/modules/gcp-carbon-pipeline/iam.tf
@@ -11,17 +11,6 @@ resource "google_organization_iam_custom_role" "bigquery_transfer_climate_data_p
   ]
 }
 
-# Permission to read carbon information from the billing account
-resource "google_organization_iam_custom_role" "bigquery_transfer_climate_data_billing_account" {
-  role_id     = "BigQueryTransferClimateBillingAccount"
-  org_id      = var.org_id
-  title       = "BigQuery Transfer for Climate Data (billing account permissions)"
-  description = "Least privilege for transferring GCP climate data into BigQuery (billing account permissions)."
-  permissions = [
-    "billing.accounts.getCarbonInformation",
-  ]
-}
-
 resource "google_organization_iam_member" "climate_project_role" {
   org_id = var.org_id
   role   = google_organization_iam_custom_role.bigquery_transfer_climate_data_project.id
@@ -30,6 +19,6 @@ resource "google_organization_iam_member" "climate_project_role" {
 
 resource "google_organization_iam_member" "climate_billing_role" {
   org_id = var.org_id
-  role   = google_organization_iam_custom_role.bigquery_transfer_climate_data_billing_account.id
+  role   = "roles/billing.carbonViewer"
   member = google_service_account.gcp_climate_data.member
 }
